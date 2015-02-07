b8536c2a264448b88d66c0ff8de59172f0190e41
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2015-02-07 16:09:15 +0900

    Lesson7 section3

---
+++ RSS-Reader/ArticleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArticleTableViewCell.swift
+//  RSS-Reader
+//
+//  Created by shinbo maiki on 2015/02/05.
+//  Copyright (c) 2015年 shinbo maiki. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
